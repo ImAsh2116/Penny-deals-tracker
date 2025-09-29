50f4753769eb6039661e70560f0439b3b2a0a265
ImAsh2116 <ashtonrsefcik21@gmail.com>
2025-09-29 16:18:39 -0400

    Update service-worker.js

--- service-worker.js
+++ service-worker.js
@@ -1,1 +1,5 @@
-const CACHE='
+const CACHE_NAME='penny-deals-cache-v3';
+const FILES_TO_CACHE=['./','./index.html','./style.css','./app.js','./manifest.json','./icon-192.png','./icon-512.png'];
+self.addEventListener('install',e=>{e.waitUntil(caches.open(CACHE_NAME).then(c=>c.addAll(FILES_TO_CACHE))); self.skipWaiting();});
+self.addEventListener('activate',e=>{e.waitUntil(caches.keys().then(keys=>Promise.all(keys.map(k=>k!==CACHE_NAME&&caches.delete(k))))); self.clients.claim();});
+self.addEventListener('fetch',e=>{ if(e.request.method!=='GET') return; e.respondWith(caches.match(e.request).then(r=>r||fetch(e.request))); });
